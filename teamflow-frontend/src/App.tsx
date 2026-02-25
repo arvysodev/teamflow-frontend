f8c7581073ada3689dd7dd59da6f822a4392a0b0
arvysodev <artjom.vysotski@devtailor.com>
2026-02-25 11:41:38 +0200

    initial Vite + React + Tailwind setup

---
+++ teamflow-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen p-6">
+      <h1 className="text-3xl font-bold underline">Tailwind works</h1>
+    </div>
+  );
+}
+
+export default App
