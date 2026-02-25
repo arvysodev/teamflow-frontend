31bb04f627de27e9d2569f61c84bef20774c44e4
arvysodev <artjom.vysotski@devtailor.com>
2026-02-25 11:54:08 +0200

    init shadcn

--- teamflow-frontend/src/App.tsx
+++ teamflow-frontend/src/App.tsx
@@ -3,10 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <div className="min-h-screen p-6">
-      <h1 className="text-3xl font-bold underline">Tailwind works</h1>
+    <div className="p-10 text-3xl font-bold text-red-500">
+      Tailwind works 🚀
     </div>
-  );
+  )
 }
 
 export default App
